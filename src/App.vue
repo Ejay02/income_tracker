660823d28bd67a0cf71c6003ac8c324fb35828dc
Ejay02 <e.jae02@gmail.com>
2022-03-29 18:30:15 +0100

    basic income tracker

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,89 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header-nav :totalIncome="state.totalIncome" />
+  <form-input :state="state" @addIncome="AddIncome" />
+  <income-list :state="state" @remove-item="removeItem" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from "@vue/reactivity";
+import HeaderNav from "./components/HeaderNav.vue";
+import { computed } from "@vue/runtime-core";
+import FormInput from "./components/FormInput.vue";
+import IncomeList from "./components/IncomeList.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { HeaderNav, FormInput, IncomeList },
+
+  setup() {
+    const state = reactive({
+      income: [],
+      totalIncome: computed(() => {
+        let temp = 0;
+        if (state.income.length > 0) {
+          for (let i = 0; i < state.income.length; i++) {
+            temp += state.income[i].value;
+          }
+        }
+        return temp;
+      }),
+
+      // Sort income by date
+      sortedIncome: computed(() => {
+        let temp = [];
+
+        temp = state.income.sort(function (a, b) {
+          return b.date - a.date;
+        });
+        // temp = state.income.sort((a, b) => {
+        //   return new Date(b.date) - new Date(a.date);
+        // });
+
+        return {
+          temp,
+        };
+      }),
+    });
+
+    function AddIncome(data) {
+      let d = data.date.split("-");
+      let newDate = new Date(d[0], d[1], d[2]);
+      state.income = [
+        ...state.income,
+        {
+          id: Date.now(),
+          desc: data.desc,
+          value: parseInt(data.value),
+          date: newDate.getTime(),
+        },
+      ];
+    }
+
+     function removeItem(id) {
+      state.income = state.income.filter(v => v.id != id);
+    }
+
+    return {
+      state,
+      AddIncome,
+      FormInput,
+      HeaderNav,
+      IncomeList,
+      removeItem
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira Sans", sans-serif;
+}
+
+body {
+  background: #eee;
 }
 </style>
---
+++ src/components/IncomeList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="income-list">
+    <!--  -->
+    <income-item v-for="data in state.income" :key="data.id" :income="data"  @remove-item="removeItem"  />
+  </div>
+</template>
+
+<script>
+import IncomeItem from "./IncomeItem.vue";
+export default {
+  components: { IncomeItem },
+  props: {
+    state: Object,
+  },
+
+  setup(props, { emit }) {
+  function removeItem(id) {
+      emit( 'remove-item', id );
+    }
+
+    return {
+      IncomeItem,
+      removeItem
+    };
+  },
+};
+</script>
+
+<style scoped>
+.income-list {
+  margin-top: 30px;
+  padding: 15px;
+}
+</style>
